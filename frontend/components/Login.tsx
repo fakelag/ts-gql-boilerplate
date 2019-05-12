94ed492d4fb7d27a8af108a9836206ccc72c9f9f
fakelag <si.harjunpaa@gmail.com>
2019-05-12 17:41:39 +0300

    Refactor login into a React FC

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import TextBox from './TextBox';
 
-interface ILoginStateProps {}
-interface ILoginActions {}
-interface ILoginState {}
-interface ILoginProps extends ILoginStateProps, ILoginActions {}
+interface ILoginProps {}
 
-class Login extends React.Component<ILoginProps, ILoginState> {
-	public state: ILoginState = {
+const Login = (prop: ILoginProps) => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const performLogin = async () => {
+		try {
+			await axios.post('/login', {
+				name: username,
+				passwd: password,
+			});
+
+			window.location.href = '/';
+		} catch (err) {
+			setError(err.message);
+		}
 	};
 
-	constructor(props: ILoginProps) {
-		super(props);
-	}
-
-	public render() {
-		return (<React.Fragment>
-			{'<Login /> -- access /login to log in'}
-		</React.Fragment>);
-	}
-}
+	return (<React.Fragment>
+		<div style={{
+				display: 'flex',
+				flexDirection: 'column',
+				height: '10rem',
+				justifyContent: 'space-between',
+				marginLeft: 'auto',
+				marginRight: 'auto',
+				marginTop: '10rem',
+				width: '20rem',
+			}}
+		>
+			<TextBox
+				isEditable
+				placeHolder="Username..."
+				startsEditing={false}
+				onChange={(value: string) => setUsername(value)}
+			/>
+			<TextBox
+				isEditable
+				placeHolder="Password..."
+				startsEditing={false}
+				onChange={(value: string) => setPassword(value)}
+			/>
+			<button className="LoginButton" onClick={performLogin}>
+				<div className="Center">Log In</div>
+			</button>
+			<div className="ErrorMessage">
+				{error}
+			</div>
+		</div>
+	</React.Fragment>);
+};
 
 export default Login;
